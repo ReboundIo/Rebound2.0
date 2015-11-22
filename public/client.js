e8f105ad80dd8a3634c24c467cc56b0efd553369
Aryan Jha <aryanjha@live.com>
2015-11-21 17:03:02 -0800

    Working basic account system
    Login system with username and password.

--- public/client.js
+++ public/client.js
@@ -21,7 +21,7 @@ $(function() {
   var connected = false;
   var typing = false;
   var lastTypingTime;
-  var $currentInput = $usernameInput.focus();
+  var $currentInput
 
   var socket = io();
 
@@ -46,8 +46,10 @@ $(function() {
       $loginPage.off('click');
       $currentInput = $inputMessage.focus();
 
+      password = document.getElementById('pwdinput').value;
+
       // Tell the server your username
-      socket.emit('add user', username);
+      socket.emit('add user', username, password);
     }
   }
 
@@ -196,13 +198,18 @@ $(function() {
     return COLORS[index];
   }
 
+  function createAccount() {
+
+    desiredUsername = prompt("What is your desired username? \nThis will show up as your name when you send messages.");
+    desiredPassword = prompt("What is your desired password?");
+    socket.emit('new user account', desiredUsername, desiredPassword);
+
+  }
+
   // Keyboard events
 
   $window.keydown(function (event) {
     // Auto-focus the current input when a key is typed
-    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
-      $currentInput.focus();
-    }
     // When the client hits ENTER on their keyboard
     if (event.which === 13) {
       if (username) {
@@ -220,11 +227,6 @@ $(function() {
   });
 
   // Click events
-
-  // Focus input when clicking anywhere on login page
-  $loginPage.click(function () {
-    $currentInput.focus();
-  });
 
   // Focus input when clicking on the message input's border
   $inputMessage.click(function () {
@@ -272,4 +274,13 @@ $(function() {
   socket.on('stop typing', function (data) {
     removeChatTyping(data);
   });
+
+  socket.on('alert', function(alertMessage) {
+    alert(alertMessage);
+  });
+
+  socket.on('alertrefresh', function(alertMessage) {
+    alert(alertMessage);
+    window.location=window.location;
+  })
 });
--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 // set up basic express server
 var express = require('express');
-var prompt = require('prompt');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://localhost:27017', ['accounts']);
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 80;
-var COLOR_NAMES = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray', 'silver', 'white', 'indianred', 'lightcoral', 'salmon', 'darksalmon', 'lightsalmon', 'crimson', 'firebrick', 'darkred', 'greenyellow', 'chartreuses', 'lawngreen', 'lime', 'limegreen', 'palegreen', 'lightgreen', 'mediumspringgreen', 'springgreen', 'mediumseagreen', 'seagreen', 'forestgreen', 'darkgreen', 'yellowgreen', 'olivedrab', 'darkolivegreen', 'mediumaquamarine','darkseagreen', 'lightseagreen', 'darkcyan', 'cornsilk', 'blanchedalmond', 'bisque', 'navajowhite', 'wheat', 'burlywood', 'tan', 'rosybrown', 'sandybrown', 'goldenrod', 'darkgoldenrod', 'peru', 'chocolate', 'saddlebrown', 'sienna', 'brown', 'maroon', 'lightsalmon', 'coral', 'tomato', 'orangered', 'darkorange', 'orange', 'gold', 'yellow', 'lightyellow', 'lemonchiffon', 'lightgoldenrodyellow', 'papayawhip', 'moccasin', 'peachpuff', 'palegoldenrod', 'khaki', 'darkkhaki', 'snow', 'honeydew', 'mintecream', 'azure', 'aliceblue', 'ghostwhite', 'whitesmoke', 'seashell', 'beige', 'oldlace', 'floralwhite', 'ivory', 'antiquewhite', 'linen', 'lavenderblush', 'mistyrose', 'aqua', 'cyan', 'lightcyan', 'paleturquoise', 'aquamarine', 'turquoise', 'mediumturquoise', 'darkturquoise', 'cadetblue', 'steelblue', 'lightsteelblue', 'powderblue', 'lightblue', 'skyblue', 'lightskyblue', 'deepskyblue', 'dodgerblue', 'cornflowerblue', 'mediumslateblue', 'royalblue', 'blue', 'mediumblue', 'darkblue', 'navy', 'midnightblue', 'lavender', 'thistle', 'plum', 'violet', 'orchid', 'magenta', 'mediumorchid', 'mediumpurple', 'amethyst', 'blueviolet', 'darkviolet', 'darkorchid', 'darkmagenta', 'purple', 'indigo', 'gainsboro', 'lightgrey', 'silver', 'darkgray', 'gray', 'dimgray', 'lightslategray', 'slategray', 'darkslategray', 'pink', 'lightpink', 'hotpink', 'deeppink', 'mediumvioletred', 'palevioletred'];
-
-prompt.start();
+var port = process.env.PORT || 3000;
 
 // tell server to start listening for connections
 server.listen(port, function () {
-
   console.log("Server listening at port %d", port);
-
 });
 
 // routing
@@ -29,123 +25,42 @@ var numUsers = 0;
 io.on('connection', function (socket) {
   var addedUser = false;
 
-  // on a new message
-  socket.on('new message', function (data) {
-    // tell the client to add a new message
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
-  });
-
   // when a new user joins
-  socket.on('add user', function (username) {
-    // store the username in the socket session for this client
-    socket.username = username;
-    // add client's username to global listen
-    usernames[username] = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo to all clients that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
+  socket.on('add user', function (username, password) {
+    db.accounts.findOne({ "username" : username, "password" : password }, function(err, docs) {
+      if (docs) {
+        // store the username in the socket session for this client
+        socket.username = username;
+        // add client's username to global listen
+        usernames[username] = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+          numUsers: numUsers
+        });
 
-  socket.on('typing', function() {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client stops typing, tell everyone else
-  socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
+        // echo to all clients that a person has connected
+        socket.broadcast.emit('user joined', {
+          username: socket.username,
+          numUsers: numUsers
+        });
+      } else {
+        socket.emit('alertrefresh', 'That user does not exist. Try again or create an account.');
+      }
     });
   });
 
-  // when someone disconnects, do this
-  socket.on('disconnect', function () {
-    // remove username from global usernames listen
-    if (addedUser) {
-      delete usernames[socket.username];
-      --numUsers;
+  socket.on('new user account', function(username, password) {
 
-      // echo to all other clients that someone left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
+    db.accounts.findOne({ "username" : username }, function(err, docs) {
+
+      if (docs) {
+        socket.emit('alertrefresh', 'That username is taken. Try again.');
+      } else {
+        db.accounts.save({ "username" : username, "password" : password });
+      }
+
+    })
+
   });
-
-});//----------- define functions for prompt commands
-
-function setColor(username, color) {
-    usernames[username].color = color;
-}
-
-function spin(username) {
-    usernames[username].emit('spin');
-    sendSystemMessage(username + " has been spun by an administrator.");
-}
-
-function kick(username) {
-    usernames[username].emit('alert', 'You have been kicked from the server.');
-    usernames[username].disconnect();
-    sendSystemMessage(username + " has been kicked.");
-}
-
-function kickAll() {
-    for (var username in usernames) {
-        kick(username);
-    }
-}
-
-//------------------------------------------------
-
-(function get() {
-    prompt.get(['command'], function(error, result) {
-        if (result.command == 'system') {
-            prompt.get(['message'], function(error, result) {
-                sendSystemMessage(result.message);
-                get();
-            });
-        }
-        /* how to add a prompt command
-        if (result.command == 'keyword here') {
-            prompt.get(['any extra information you might need'], function(error, result) {
-              someFunction(result.yourExtraInfo);
-              get();
-            });
-          }
-        */
-        if (result.command == 'kick') {
-            prompt.get(['username'], function(error, result) {
-                kick(result.username);
-                get();
-            });
-        }
-        if (result.command == 'color') {
-            prompt.get(['username', 'color'], function(error, result) {
-                setColor(result.username, result.color)
-                get();
-            });
-        }
-        if (result.command == 'spin') {
-            prompt.get(['username'], function(error, result) {
-                spin(result.username);
-                get();
-            });
-        }
-        if (result.command == 'kickall') {
-            kickAll();
-            get();
-        }
-    });
-})();
+});
